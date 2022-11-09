461a06c9685a4e995de1211498845f580b3d876e
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-11-09 11:04:08 +0200

    Add

--- 02. Asynchronous Programming/Exercise/02.Bus-Schedule/app.js
+++ 02. Asynchronous Programming/Exercise/02.Bus-Schedule/app.js
@@ -1,32 +1,65 @@
-function solve() {
+// function solve() {
 
-    let baseUrl = 'http://localhost:3030/jsonstore/bus/schedule';
-    let infoDiv = document.querySelector('.info');
+//     let baseUrl = 'http://localhost:3030/jsonstore/bus/schedule';
+//     let infoDiv = document.querySelector('.info');
+//     let departBtn = document.getElementById('depart');
+//     let arriveBtn = document.getElementById('arrive');
+
+//     let stopId = 'depot';
+//     let stopName = '';
+
+//     function depart() {
+//         fetch(`${baseUrl}/${stopId}`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 stopName = data.name;
+
+//                 infoDiv.textContent = `Next stop ${stopName}`;
+//                 stopId = data.next;
+
+//                 departBtn.disabled = true;
+//                 arriveBtn.disabled = false;
+//             })
+//     }
+
+//     function arrive() {
+//         infoDiv.textContent = `Arriving at ${stopName}`;
+
+//         departBtn.disabled = false;
+//         arriveBtn.disabled = true;
+//     }
+
+//     return {
+//         depart,
+//         arrive
+//     };
+// }
+
+function solve() {
+    let label = document.querySelector('#info span');
     let departBtn = document.getElementById('depart');
     let arriveBtn = document.getElementById('arrive');
+    let stop = {
+        next: 'depot'
+    };
 
-    let stopId = 'depot';
-    let stopName = '';
+    async function depart() {
 
-    function depart() {
-        fetch(`${baseUrl}/${stopId}`)
-            .then(response => response.json())
-            .then(data => {
-                stopName = data.name;
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
 
-                infoDiv.textContent = `Next stop ${stopName}`;
-                stopId = data.next;
+        let res = await fetch(url);
+        stop = await res.json();
 
-                departBtn.disabled = true;
-                arriveBtn.disabled = false;
-            })
+        label.textContent = `Next stop ${stop.name}`;
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        infoDiv.textContent = `Arriving at ${stopName}`;
+        label.textContent = `Arriving at ${stop.name}`;
 
-        departBtn.disabled = false;
         arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
 
     return {
