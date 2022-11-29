0e98f6dfffe7cb370629d39c28381085117637f9
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-11-29 21:53:47 +0200

    Delete Exam Preparation/Sole Mates directory

--- Exam Preparation/Sole Mates/middlewares/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-import * as sessionService from '../services/authService.js';
-
-export function saveUserStatus(ctx, next){
-    ctx.user = sessionService.getUser();
-    next();
-}
--- Exam Preparation/Sole Mates/services/authService.js
+++
@@ -1,9 +0,0 @@
-export function getUser() {
-    return JSON.parse(sessionStorage.getItem('userData'));
-};
-
-export function setUser(data) {
-    sessionStorage.setItem('userData', JSON.stringify(data));
-};
-
-export function removeUser() { sessionStorage.clear(); }
